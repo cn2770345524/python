b0b5a3de2c707b2e6967e05438a84f71f1e04db9
cn2770345524 <cn2770345524@qq.com>
2023-09-25 17:39:11 +0800

    init

---
+++ 输入输出.py
@@ -0,0 +1,15 @@
+print('hello world')
+
+print('hello world','你好','今天是星期一',sep='，',end='\t')
+
+print('hello world')
+
+print(200)
+
+print(200+300)
+
+print('200+300=',200+300,sep='')
+
+name=input('请输入您的姓名\n')
+print('name=',name)
+
